3cdaec31b78a11c8af8ff6e7034b8e7b5789db61
audgh3364 <audgh3364@naver.com>
2018-04-27 16:21:50 +0900

    2018-04-27
    관리자 회원통계 진행중

--- mbs/src/main/java/com/mbs/mvc/controller/V1_AdminChartController.java
+++ mbs/src/main/java/com/mbs/mvc/controller/V1_AdminChartController.java
@@ -1,5 +1,7 @@
 package com.mbs.mvc.controller;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,10 @@ public class V1_AdminChartController {
 	
 	@RequestMapping(value="admin_memberchart.do",method=RequestMethod.GET)
 	public String adminmemberchart() {
-		
+		 Calendar oCalendar = Calendar.getInstance( );
+		 System.out.println("현재 년: " +  oCalendar.get(Calendar.YEAR));
+		 System.out.println("현재 월: " + (oCalendar.get(Calendar.MONTH) + 1));
+		 System.out.println("현재 일: " +  oCalendar.get(Calendar.DAY_OF_MONTH));
 		return "v1_admin_memberchart";
 	}
 }
